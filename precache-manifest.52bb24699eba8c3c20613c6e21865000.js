6b5829b1b2b4e5e6f955ebee28c8596a1656e1c6
Saurabh Gaur <saurabh.gaur01@gmail.com>
2021-08-03 21:45:33 +0100

    Updates

---
+++ precache-manifest.52bb24699eba8c3c20613c6e21865000.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f5e9d321eef0c9db4fcee4a9b0a2497",
+    "url": "/todo-application/index.html"
+  },
+  {
+    "revision": "8f1c0069744ebd3250e5",
+    "url": "/todo-application/static/css/2.dec86d71.chunk.css"
+  },
+  {
+    "revision": "c0377a87b40eabe8a591",
+    "url": "/todo-application/static/css/main.41f2d412.chunk.css"
+  },
+  {
+    "revision": "8f1c0069744ebd3250e5",
+    "url": "/todo-application/static/js/2.90ad973d.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/todo-application/static/js/2.90ad973d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0377a87b40eabe8a591",
+    "url": "/todo-application/static/js/main.a40f08b6.chunk.js"
+  },
+  {
+    "revision": "126e38d63b240a1a199c",
+    "url": "/todo-application/static/js/runtime-main.3993849e.js"
+  }
+]);
